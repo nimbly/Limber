a4d788fc40c271f0802fab723eea1d42f209f205
Brent Scheffler <brent@nimbly.io>
2019-09-09 12:25:04 -0700

    Adding test coverage for EmptyStream.

---
+++ tests/EmptyStreamTest.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace Limber\Tests;
+
+use Limber\EmptyStream;
+use PHPUnit\Framework\TestCase;
+use RuntimeException;
+
+/**
+ * @covers Limber\EmptyStream
+ */
+class EmptyStreamTest extends TestCase
+{
+	public function test_casting_to_string_returns_empty_string()
+	{
+		$emptyStream = new EmptyStream;
+		$this->assertEquals("", (string) $emptyStream);
+	}
+
+	public function test_get_size_returns_zero()
+	{
+		$emptyStream = new EmptyStream;
+		$this->assertEquals(0, $emptyStream->getSize());
+	}
+
+	public function test_tell_returns_zero()
+	{
+		$emptyStream = new EmptyStream;
+		$this->assertEquals(0, $emptyStream->tell());
+	}
+
+	public function test_eof_returns_true()
+	{
+		$emptyStream = new EmptyStream;
+		$this->assertTrue($emptyStream->eof());
+	}
+
+	public function test_is_seekable_returns_false()
+	{
+		$emptyStream = new EmptyStream;
+		$this->assertFalse($emptyStream->isSeekable());
+	}
+
+	public function test_seek_throws_runtime_exception()
+	{
+		$emptyStream = new EmptyStream;
+
+		$this->expectException(RuntimeException::class);
+		$emptyStream->seek(1);
+	}
+
+	public function test_rewind_throws_runtime_exception()
+	{
+		$emptyStream = new EmptyStream;
+
+		$this->expectException(RuntimeException::class);
+		$emptyStream->rewind();
+	}
+
+	public function test_is_writeable_returns_false()
+	{
+		$emptyStream = new EmptyStream;
+		$this->assertFalse($emptyStream->isWritable());
+	}
+
+	public function test_write_throws_runtime_exception()
+	{
+		$emptyStream = new EmptyStream;
+
+		$this->expectException(RuntimeException::class);
+		$emptyStream->write("foo");
+	}
+
+	public function test_is_readable_returns_false()
+	{
+		$emptyStream = new EmptyStream;
+		$this->assertTrue($emptyStream->isReadable());
+	}
+
+	public function test_read_returns_empty_string()
+	{
+		$emptyStream = new EmptyStream;
+		$this->assertEquals("", $emptyStream->read(12));
+	}
+
+	public function test_get_contents_returns_empty_string()
+	{
+		$emptyStream = new EmptyStream;
+		$this->assertEquals("", $emptyStream->getContents());
+	}
+
+	public function test_get_metadata_returns_null()
+	{
+		$emptyStream = new EmptyStream;
+		$this->assertNull($emptyStream->getMetadata());
+	}
+}
